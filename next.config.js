3bd84a1636ee59682a179c90c90c59f75a9f739a
Phumrapee Limpianchop <git@rayriffy.com>
2019-11-01 02:11:31 +0700

    :wrench: Next configuration

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
-const withPlugins = require('next-compose-plugins')
-const optimizedImages = require('next-optimized-images')
-
-const core = {
+const config = {
   assetPrefix: process.env.NODE_ENV === 'production' ? '/ywc17-chimp' : '',
 }
-
-const config = withPlugins([
-  [optimizedImages, {
-    handleImages: ['jpeg', 'png', 'webp'],
-    optimizeImagesInDev: true,
-    mozjpeg: {
-      quality: 80,
-    },
-    optipng: {
-      optimizationLevel: 3,
-    },
-    webp: {
-      preset: 'default',
-      quality: 75,
-    },
-  }],
-], core)
 
 module.exports = config
