bdd5109415438643ceba7a4223dc7a534660e8e3
Phumrapee Limpianchop <git@rayriffy.com>
2019-11-01 18:33:25 +0700

    :recycle: Refactor

--- src/templates/home/components/contact.tsx
+++ src/templates/home/components/contact.tsx
@@ -5,31 +5,37 @@ import { Box, Flex, Image, Link } from 'rebass'
 import { assetURL } from '../../../core/services/assetURL'
 
 const HomeContactComponent: React.FC = props => {
+  const contacts = [
+    {
+      code: 'ktb',
+      href: 'tel:021111144',
+    },
+    {
+      code: 'cgd',
+      href: 'tel:022706400',
+    },
+    {
+      code: 'tat',
+      href: 'tel:1672',
+    },
+  ]
+
   return (
     <Flex justifyContent={`center`}>
       <Box width={[22 / 24, 20 / 24, 18 / 24]} py={4}>
         <Flex flexWrap={`wrap`}>
-          <Box width={[1, 1 / 3]} p={4}>
-            <Flex justifyContent={`center`} alignItems={`center`}>
-              <Link href={`tel:021111144`}>
-                <Image src={`${assetURL}/static/images/contact/ktb.png`} />
-              </Link>
-            </Flex>
-          </Box>
-          <Box width={[1, 1 / 3]} p={4}>
-            <Flex justifyContent={`center`} alignItems={`center`}>
-              <Link href={`tel:022706400`}>
-                <Image src={`${assetURL}/static/images/contact/cgd.png`} />
-              </Link>
-            </Flex>
-          </Box>
-          <Box width={[1, 1 / 3]} p={4}>
-            <Flex justifyContent={`center`} alignItems={`center`}>
-              <Link href={`tel:1672`}>
-                <Image src={`${assetURL}/static/images/contact/tat.png`} />
-              </Link>
-            </Flex>
-          </Box>
+          {contacts.map(contact => {
+            const {code, href} = contact
+            return (
+              <Box width={[1, 1 / 3]} p={4}>
+                <Flex justifyContent={`center`} alignItems={`center`}>
+                  <Link href={href}>
+                    <Image src={`${assetURL}/static/images/contact/${code}.png`} />
+                  </Link>
+                </Flex>
+              </Box>
+            )
+          })}
         </Flex>
       </Box>
     </Flex>
