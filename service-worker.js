1593fefd78004d714a70b22f4a3151644890a07c
rayriffy <rayriffy@users.noreply.github.com>
2020-07-19 03:53:03 +0000

    Automated deployment: Sun Jul 19 03:53:03 UTC 2020 fce0d7b3440f7e11ae8fe6a6b11b9460734dbaa4

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,51 @@
 self.__precacheManifest = [
   {
-    "url": "/ywc17-chimp/_next/static/_K30lOzBAP05QcAcmQrru/pages/_app.js",
-    "revision": "57fe627d121737cb4ce0"
+    "url": "/ywc17-chimp/_next/static/chunks/1bfc9850.a4ba1d10ea75b4e49bac.js",
+    "revision": "99ca2f4d70eb2fcc0283"
   },
   {
-    "url": "/ywc17-chimp/_next/static/_K30lOzBAP05QcAcmQrru/pages/_error.js",
-    "revision": "53e9ff097e75c9a88862"
+    "url": "/ywc17-chimp/_next/static/chunks/bf206722f03c9d9a0d3e16ea23cc5afb37777035.9b227f04f502c3b11964.js",
+    "revision": "144d2d98137e57fe048f"
   },
   {
-    "url": "/ywc17-chimp/_next/static/_K30lOzBAP05QcAcmQrru/pages/index.js",
-    "revision": "8aa52dfa69c67d02d6c5"
+    "url": "/ywc17-chimp/_next/static/chunks/e96e01d01ed113ec07980be6846e99e22ef1ef2d.b1683ee4e532dba9260b.js",
+    "revision": "9650b8fd707efe51db43"
   },
   {
-    "url": "/ywc17-chimp/_next/static/chunks/commons.0aba2d40f94867824fdd.js",
-    "revision": "6bac114fbf6face105b8"
+    "url": "/ywc17-chimp/_next/static/chunks/framework.a90f5a071e35be374b29.js",
+    "revision": "bb83b3acd1c982cb7f5c"
   },
   {
-    "url": "/ywc17-chimp/_next/static/runtime/main-ca081a6ce9de9493c02b.js",
-    "revision": "c7349f3ecfff5d1b58bd"
+    "url": "/ywc17-chimp/_next/static/kv9CYDcCxCXrPP_Y0rdG3/_buildManifest.js",
+    "revision": "fb96ae7926f5104f50f0cf1b3a23a9b5"
   },
   {
-    "url": "/ywc17-chimp/_next/static/runtime/webpack-035ac2b14bde147cb4a8.js",
-    "revision": "be4b6cc6d10632d2262c"
+    "url": "/ywc17-chimp/_next/static/kv9CYDcCxCXrPP_Y0rdG3/_ssgManifest.js",
+    "revision": "abee47769bf307639ace4945f9cfd4ff"
+  },
+  {
+    "url": "/ywc17-chimp/_next/static/kv9CYDcCxCXrPP_Y0rdG3/pages/_app.js",
+    "revision": "8148aadb721e4098b017"
+  },
+  {
+    "url": "/ywc17-chimp/_next/static/kv9CYDcCxCXrPP_Y0rdG3/pages/_error.js",
+    "revision": "52ef6fe92e8190b6f7f8"
+  },
+  {
+    "url": "/ywc17-chimp/_next/static/kv9CYDcCxCXrPP_Y0rdG3/pages/index.js",
+    "revision": "844f1e8983589cd98e42"
+  },
+  {
+    "url": "/ywc17-chimp/_next/static/runtime/main-759cb373824f7531191a.js",
+    "revision": "6233aca9012b3eaf1e01"
+  },
+  {
+    "url": "/ywc17-chimp/_next/static/runtime/polyfills-934ee1c38f9bd83fb20d.js",
+    "revision": "df490ee85201423a56cf"
+  },
+  {
+    "url": "/ywc17-chimp/_next/static/runtime/webpack-b65cab0b00afd201cbda.js",
+    "revision": "04cb911a52af21884f6c"
   }
 ];
 
